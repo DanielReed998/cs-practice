6c6afb364e3f0290fc3f51fb847d8e5777033d77
Daniel Reed <daniel.reed998@gmail.com>
2017-10-28 16:33:17 -0500

    abstracted everything to functions and added documentation

--- hashtables/hashtable.js
+++ hashtables/hashtable.js
@@ -1,24 +1,64 @@
-function hash(hashtable, element, hashSize) {
-    if (typeof hashtable === 'object') {
+//Takes a predefined hash table (hashTable), an element to be inserted into 
+//that hashtable (element), and the number of separate keys the hash table
+//will be divided into (numDiv);
+function hash(hashTable, element, numDiv) {
+    if (typeof hashTable === 'object') {
         var hashSum = 0;
         for (var char of element) {
             hashSum += char.charCodeAt(0);
         }
-        if (hashTable[hashSum % hashSize]) {
-            hashTable[hashSum % hashSize].push(element);
+        if (hashTable[hashSum % numDiv]) {
+            hashTable[hashSum % numDiv].push(element);
         } else {
-            hashTable[hashSum % hashSize] = [element];
+            hashTable[hashSum % numDiv] = [element];
         }
+    } else {
+        throw new Error('Not a hashtable');
     }
 }
 
-var hashTable = {};
-var wordArray = ['hello', 'hi', 'greetings', 'dog', 'cat', 'gerbil', 'Hoagie', 'this is tedious',
-    'should have made a separate function for this', 'animal', 'another animal'
-];
-
-for (var word of wordArray) {
-    hash(hashTable, word, 10);
+//Creates an array of random strings made up of uppercase alphabet letters.
+// maxStringLength is the max length of any given random string. 
+// arrayLength is the length of the generated array.
+function randomStringArray(maxStringLength, arrayLength) {
+    var out = [];
+    while (out.length < arrayLength) {
+        var newString = '';
+        var stringLength = Math.ceil(Math.random() * maxStringLength);
+        for (var i = 0; i < stringLength; i++) {
+            //char codes for alphabet = 65 - 90.
+            var charCode = Math.floor(Math.random() * 25) + 65;
+            newString += String.fromCharCode(charCode);
+        }
+        out.push(newString);
+    }
+    return out;
 }
 
-console.log(hashTable);
+//creates and returns a new hash table from a given array. takes the array (arr), and the 
+//number of separate keys the hash table will be divided into (numDiv)
+function createHashTable(arr, numDiv) {
+    var hashTable;
+    for (var element of arr) {
+        hash(hashTable, element, numDiv);
+    }
+    return hashTable;
+}
+
+//takes a hash table as an argument (hashTable), and returns another object showing how many elements 
+// are in each key of that hash table.
+function hashTableValues(hashTable) {
+    var output = {};
+    var i = 0;
+    for (var key of Object.keys(hashTable)) {
+        output[i] = hashTable[key].length;
+        i++;
+    }
+    return output;
+}
+
+var stringArray = randomStringArray(15, 10000);
+var hashTable = createHashTable(stringArray, 25);
+var values = hashTableValues(hashTable);
+
+console.log(values);
